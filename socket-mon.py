79624fe705621ce8b2dc607b9d60bb5a2ff29e8b
TanmayAB <tabhatt1993@gmail.com>
2017-02-14 00:30:25 -0800

    Adding Lab-1 files

---
+++ socket-mon.py
@@ -0,0 +1,102 @@
+import psutil
+
+# def findChar(command_output, required, n):
+#     parts= command_output.split(required, n+1)
+#     if len(parts)<=n+1:
+#         return -1
+#     return len(command_output)-len(parts[-1])-len(required)
+
+
+conns= psutil.net_connections('tcp');
+# for conn in conns:
+# 	print conn;
+# 	print "\n"
+mylist = [];
+i = 0;
+j = 0;
+k=0;
+
+for conn in conns:
+	laddr_flag = 0;
+	raddr_flag = 0;
+	laddr = '';
+	raddr = '';
+	pid=getattr(conn,'pid');
+	if pid==None:
+		print "Encountered none";
+		pid=0;
+		print pid;
+	laddr_tuple=getattr(conn,'laddr');
+	raddr_tuple=getattr(conn,'raddr');
+	status=getattr(conn,'status');
+	# print "\n"
+	# print pid;
+	# print "laddr : " + str(laddr_tuple);
+	# print "raddr : " + str(raddr_tuple);
+	i=i+1;
+	if laddr_tuple:
+		laddr_flag = 1;
+		laddr= laddr_tuple[0] + '@' + str(laddr_tuple[1]);
+		# print "i is + " + str(i) + "flag for laddr : " + str(laddr_flag);
+		# print "\n"
+	if raddr_tuple:
+		raddr_flag = 1;
+		raddr= raddr_tuple[0] + '@' + str(raddr_tuple[1]);
+		# print "i is" + str(i) + "flag for raddr : " + str(raddr_flag);
+		# print "\n"
+	if laddr_flag==1 or raddr_flag ==1:
+		k = k + 1;
+		mylist.append([pid,laddr,raddr,status]);
+	else:
+		j=j+1;
+		# print "\n";
+print "My dict is : \n";
+mylist.sort();
+print ("\"Pid\", \"Laddr\", \"raddr\", \"Status\"");
+for value in mylist:
+		print ("\"%d\", \"%s\", \"%s\", \"%s\"" % (value[0],value[1],value[2],value[3]));
+
+# print "Mylist length : " + str(len(mylist));
+# print "For loop length : " + str(i);
+# print "How many times both were true : " + str(k);
+# print "Value not present length : " + str(j);
+
+	# laddr_start= findChar(conn_str,'(',1);
+	# laddr_end= findChar(conn_str,')',0);
+	# if laddr_end-laddr_start==1:
+	# 	laddr = '';
+	# else:
+	# 	laddr_start= findChar(conn_str,'(',1);
+	# 	laddr_end= findChar(conn_str,'\'',1);
+	# 	laddr_start= laddr_start + 2;
+	# 	laddr_end= laddr_end;
+	# 	laddr = conn_str[laddr_start:laddr_end];
+
+	# raddr_start= findChar(conn_str,'(',2);
+	# raddr_end= findChar(conn_str,')',1);
+	# if (raddr_end-raddr_start)== 1:
+	# 	raddr = '';
+	# else:
+	# 	if laddr_end-laddr_start > 1:
+	# 		raddr_end= findChar(conn_str,'\'',3);
+	# 	else:
+	# 		raddr_end= findChar(conn_str,'\'',1);
+	# 	raddr_start= raddr_start + 2;
+	# 	raddr_end= raddr_end;
+	# 	raddr= conn_str[raddr_start:raddr_end];
+
+	# pid_start= findChar(conn_str,'pid',0);
+	# pid_end= findChar(conn_str,')',2);
+	# pid_start= pid_start + 4;
+	# pid_end= pid_end;
+	# pid= conn_str[pid_start:pid_end];
+	
+	# status_start= findChar(conn_str,'status',0);
+	# status_end= findChar(conn_str,'pid',0);
+	# status_start= status_start + 8;
+	# status_end= status_end - 3;
+	# status= conn_str[status_start:status_end];
+	# print "raddr : " + raddr;	
+	# print "laddr : " + laddr;
+	# print "pid : " + pid;
+	# print "status : " + status;
